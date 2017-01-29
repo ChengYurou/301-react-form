2d73dae2aa313afb023907a0f69f067ea171d217
chengyurou <18829292695@163.com>
2017-01-29 18:51:57 +0800

    finish html and css

--- client/src/component/PaperEditor.js
+++ client/src/component/PaperEditor.js
@@ -50,8 +50,7 @@ export default class PaperEditor extends Component {
             <div className='split-border'></div>
             <div className="row">
               <div className='col-sm-offset-1 col-sm-10 no-padding'>
-
-                <div className="section-header row">
+                <div className="section-header">
                   <div className='section-title col-sm-6 row'>
                     <div className='col-sm-3 no-padding '>
                       <input type='text' className="form-control"/>
@@ -65,12 +64,29 @@ export default class PaperEditor extends Component {
                   </div>
                 </div>
 
-                <div className="col-sm-offset-1 col-sm-10 no-padding">
-                  <div className='text-center'>
-                    <i className='fa fa-plus'> </i>
+                <div className='homework-border row'>
+                  <div className='add-homework'>
+                    <div className='col-sm-2'>
+                      <i className='fa fa-plus'></i>
+                    </div>
                   </div>
                 </div>
               </div>
+
+              <div className="col-sm-offset-1 col-sm-10 no-padding">
+                <div className='text-center add-section'>
+                  <i className='fa fa-plus'> </i>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div id="paper-submit" className="row">
+            <div className='col-sm-5 col-sm-offset-1 text-right'>
+              <button className='btn btn-primary btn-size'>保存</button>
+            </div>
+            <div className='col-sm-5 text-left'>
+              <button className='btn btn-primary btn-size'>发布</button>
             </div>
           </div>
         </div>
